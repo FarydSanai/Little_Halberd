523f8259d3227d478a02a97bbed9ced870390fac
Faryd Sanai <faryd149@gmail.com>
2021-06-01 18:31:37 +0300

    Add Start menu with buttons

---
+++ Assets/Little_Halberd/Game_Components/Start_Menu/StartMenuButtons.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace LittleHalberd
+{
+    public class StartMenuButtons : MonoBehaviour
+    {
+        public bool StartMenuIsActive = true;
+        [SerializeField] private GameObject StartMenuObj;
+        [SerializeField] private GameObject ControlKeysObj;
+        public void StartGame()
+        {
+            SceneManager.LoadScene("Level_1");
+        }
+        public void SetMenuWindow()
+        {
+            if (StartMenuIsActive)
+            {
+                StartMenuObj.SetActive(false);
+                StartMenuIsActive = false;
+                ControlKeysObj.SetActive(true);
+            }
+            else
+            {
+                StartMenuObj.SetActive(true);
+                StartMenuIsActive = true;
+                ControlKeysObj.SetActive(false);
+            }
+        }
+    }
+}
